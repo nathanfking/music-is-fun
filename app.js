2e66382b76165d3335e1ea3becfd93cb6417b4ed
Nathan King <nathan.f.king@gmail.com>
2016-09-29 09:14:29 -0600

    newest

--- app.js
+++ app.js
@@ -8,25 +8,31 @@ function getMusic() {
 function drawSongs(songList) {
     var template = "";
     var songListElement = document.getElementById("tunage")
-    for (var i = 0; i < songList.length; i++) {
+    for (var i = 0; i < 50; i++) {
         var tunage = songList[i]
-        template += `<div id="tunage">
-        <div class="track">
-            <div class="img-container">
-                <img id="albumArt" src="${tunage.albumArt}" alt="" />
-            </div>
-            <div class="text-container">
-                <h1>${tunage.title}</h1>
-                <p>${tunage.artist}</p>
-                <p>${tunage.collection}</p>
-                <p>${tunage.price}</p>
-            </div>
-        </div>
-    </div>`
+        template += `
+               <div class="col-sm-12 col-md-6 col-lg-4 col-lg-push-1 card">
+                <img class="background-image" id="albumArt" src="${tunage.albumArt}" alt="" /></img>
+                <li class="row-fluid">${tunage.artist}</li>
+                <li class="row-fluid">${tunage.title}</li>
+                <li class="row-fluid">${tunage.collection}</li>
+                <li class="row-fluid">${tunage.price}</li>
+                <audio class="sound" controls style="width=250px";  src="${tunage.preview} "><a href="${tunage.preview} " class="card-link ">Card link</a></audio>
+            </div>`
     }
     songListElement.innerHTML = template
 
 
     console.log(songList)
 }
-
+document.addEventListener("play", function (event) {
+    var selected = document.getElementsByClassName("sound");
+    for (var i = 0; i < selected.length; i++) {
+        var currentPlayer = selected[i]
+        if (currentPlayer != event.target) {
+            currentPlayer.pause()
+            
+     
+        }
+    }
+},true);
